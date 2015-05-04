dc92aa1f46f53ff673d03e4790a27542de44fb82
tibimosu <tibimosu1128@gmail.com>
2015-05-05 03:42:20 +0900

    うまくいかないから修正

--- scripts/showtask.js
+++ scripts/showtask.js
@@ -25,7 +25,7 @@ function mainProcess (msg) {
 	var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
 	t.get("/1/boards/" + process.env.HUBOT_TRELLO_BOARD + "/lists", function (err, data) {
 		var found = _.find(data, function (datum) {
-			msg.send(_s(datum.name).trim().value() + ", " + _s(listName).trim().value());
+			msg.send(_s(datum.name).trim().capitalize().value() + ", " + _s(listName).capitalize().trim().value());
 			return _s(datum.name).trim() === _s(listName).trim()
 		});
 		if (err || !found) {
