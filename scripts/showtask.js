faaa35b59666af2ad5eee0095fb4bfffa5937b09
tibimosu <tibimosu1128@gmail.com>
2015-05-05 00:09:55 +0900

    デバッグ中

--- scripts/showtask.js
+++ scripts/showtask.js
@@ -1,43 +1,30 @@
-// /**
-//  * Filename: showtask.js
-//  * Description: 指定したリスト名のタスクを表示します
-//  * Command: show <listname> 
-//  */
+/**
+ * Filename: showtask.js
+ * Description: 指定したリスト名のタスクを表示します
+ * Command: show <listname> 
+ */
 
-// var Trello = require("node-trello");
-// var _ = require("underscore");
+var Trello = require("node-trello");
+var _ = require("underscore");
 
-// // function getList (err, data) {
-// // 	msg.send("test");
-// // 	var res = "";
-// // 	if (err) {
-// // 		return "ERROR";
-// // 	}
-// // 	_.each (data, function (datum) {
-// // 		res += datum.name + "\n";
-// // 	});
-// // 	return res;
-// // }
+function mainProcess (msg) {
+	var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
+	var list = "";
+	t.get("/1/boards/" + process.env.HUBOT_TRELLO_BOARD + "/lists", function (err, data) {
+		if (err) {
+			msg.send("ERROR");
+			return;
+		}
+		msg.send("test");
+		_.each(data, function (datum) {
+			list += datum.name;
+		});
+	});
+	msg.send(list);
+}
 
-// function mainProcess (msg) {
-// 	// var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
-// 	// var list = "";
-// 	// t.get("/1/boards/" + process.env.HUBOT_TRELLO_BOARD + "/lists", function (err, data) {
-// 	// 	if (err) {
-// 	// 		msg.send("ERROR");
-// 	// 		return;
-// 	// 	}
-// 	// 	msg.send(data);
-// 	// 	_.each(data, function (datum) {
-// 	// 		list += datum.name;
-// 	// 	});
-// 	// 	return;
-// 	// });
-// 	msg.send("test");
-// }
-
-// module.exports = function (robot) {
-// 	robot.respond(/show/i, function (msg) {
-// 		mainProcess(msg);
-// 	});
-// }
+module.exports = function (robot) {
+	robot.respond(/show/i, function (msg) {
+		mainProcess(msg);
+	});
+}
