7ee626087c625f9a346a826685a9fda918e34638
tibimosu <tibimosu1128@gmail.com>
2015-05-04 23:54:16 +0900

    showコマンドのバグを修正

--- scripts/showtask.js
+++ scripts/showtask.js
@@ -7,21 +7,31 @@
 var Trello = require("node-trello");
 var _ = require("underscore");
 
-function getList (err, data) {
-	msg.send("test");
-	var res = "";
-	if (err) {
-		return "ERROR";
-	}
-	_.each (data, function (datum) {
-		res += datum.name + "\n";
-	});
-	return res;
-}
+// function getList (err, data) {
+// 	msg.send("test");
+// 	var res = "";
+// 	if (err) {
+// 		return "ERROR";
+// 	}
+// 	_.each (data, function (datum) {
+// 		res += datum.name + "\n";
+// 	});
+// 	return res;
+// }
 
 function mainProcess (msg) {
 	var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
-	var list = t.get("/1/boards/" + process.env.HUBOT_TRELLO_BOARD + "/lists", getList(err, data));
+	var list = "";
+	t.get("/1/boards/" + process.env.HUBOT_TRELLO_BOARD + "/lists", function (err, data) {
+		if (err) {
+			msg.send("ERROR");
+			return;
+		}
+		_each(data, function (datum) {
+			list += datum.name;
+		});
+		return;
+	});
 	return msg.send(list);
 }
 
