310c0c06fb68b7e49f0c4bf6b1538b102ee0b7a6
tibimosu <tibimosu1128@gmail.com>
2015-05-04 23:44:54 +0900

    testの追加

--- scripts/showtask.js
+++ scripts/showtask.js
@@ -20,13 +20,15 @@ function getList (err, data) {
 }
 
 function mainProcess (msg) {
-	var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
-	var list = t.get("/1/boards/" + process.env.HUBOT_TRELLO_BOARD + "/lists", getList(err, data));
-	msg.send(list);
+	// var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
+	// var list = t.get("/1/boards/" + process.env.HUBOT_TRELLO_BOARD + "/lists", getList(err, data));
+	// msg.send(list);
+	return msg.send("test");
 }
 
 module.exports = function (robot) {
-	robot.respond(/^show (.*)/i, function (msg) {
-		mainProcess(msg);
+	robot.respond(/show/i, function (msg) {
+		// mainProcess(msg);
+		msg.send("test");
 	});
 }
