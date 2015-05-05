953964dd88f5c2eb5c9be4a9d06822b15350ed42
tibimosu <tibimosu1128@gmail.com>
2015-05-05 21:03:31 +0900

    fsをうまく処理できるようにした

--- scripts/checknewcomment.js
+++ scripts/checknewcomment.js
@@ -28,11 +28,10 @@ function checkNewComment (msg, old, now) {
 	var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
 	t.get(path, function (err, data) {
 		var fs = _.filter(data, function (datum) {return old < datum.date && datum.date <= now});
-		msg.send(fs + "test");
 		if (err) {
 			msg.send("ERROR");
 			return;
-		} else if (fs === null) {
+		} else if (fs === []) {
 			msg.send("新しいコメントはありませんでした。");
 		} else {
 			_.each(fs, function (f) {
