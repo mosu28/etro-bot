0f953755f17f6ba46fba61b5d4ca13287065dc80
tibimosu <tibimosu1128@gmail.com>
2015-05-06 14:43:34 +0900

    時間がずれていたため修正

--- scripts/checknewcomment.js
+++ scripts/checknewcomment.js
@@ -19,17 +19,36 @@ function getDate (n) {
 	var hour = _s(date.getHours() + "").pad(2, "0").value();
 	var minute = _s(date.getMinutes() + "").pad(2, "0").value();
 	var second = _s(date.getSeconds() + "").pad(2, "0").value();
+	var ms = _s(date.getMilliseconds() + "").pad(3, "0").value();
 	var d = _s.join("-", year, month, day);
-	var t = _s.join(":", hour, minute, second) + ".000Z";
+	var t = _s.join(":", hour, minute, second) + "." + ms + "Z";
 	return d + "T" + t;
 }
 
-//TODO 60字ごとに改行を入れる
+function formatDate (date) {
+	var ds = [], ts = [], temp = [], ms;
+	temp = _s.words(date, "T");
+	ds = _s.words(temp[0], "-");
+	temp = _s.words(temp[1], ".");
+	ts = _s.words(temp[0], ":");
+	ms = temp[1].replace(/Z*/g, "");
+	var d = new Date(parseInt(ds[0]),parseInt(ds[1]),parseInt(ds[2]),parseInt(ts[0]) + 17,parseInt(ts[1]),parseInt(ts[2]),parseInt(ms));
+	var year = d.getFullYear() + "";
+	var month = _s(d.getMonth() + 1 + "").pad(2, "0").value();
+	var day = _s(d.getDate() + "").pad(2, "0").value();
+	var hour = _s(d.getHours() + "").pad(2, "0").value();
+	var minute = _s(d.getMinutes() + "").pad(2, "0").value();
+	var second = _s(d.getSeconds() + "").pad(2, "0").value();
+	var ms = _s(d.getMilliseconds() + "").pad(3, "0").value();
+	return _s.join("-", year, month, day) + "T" + _s.join(":", hour, minute, second) + "." + ms + "Z";
+}
 
 function checkNewComment (msg, old, now) {
 	var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
 	t.get(path, function (err, data) {
-		var fs = _.filter(data, function (datum) {return old < datum.date && datum.date <= now});
+		var fs = _.filter(data, function (datum) {
+			return old < datum.date && datum.date <= now
+		});
 		if (err) {
 			msg.send("ERROR");
 			return;
@@ -43,7 +62,7 @@ function checkNewComment (msg, old, now) {
 				msg.send("---\t" + f.memberCreator.fullName + "より新しいコメントがありました。\t---");
 				msg.send("*List:*\t" + f.data.list.name);
 				msg.send("*Card:*\t" + f.data.card.name);
-				msg.send("*Date:*\t" + f.date);
+				msg.send("*Date:*\t" + formatDate(f.date));
 				msg.send("*Comment: *\n\t" + f.data.text);
 			});
 		}
