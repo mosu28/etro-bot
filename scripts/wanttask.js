86a42f23ba7a11932f45aefc11e8649b82e57627
tibimosu <tibimosu1128@gmail.com>
2015-05-08 11:34:34 +0900

    want機能の修正

--- scripts/wanttask.js
+++ scripts/wanttask.js
@@ -11,9 +11,9 @@ function wantTask (msg) {
 	var t = new Trello(process.env.HUBOT_TRELLO_KEY, process.env.HUBOT_TRELLO_TOKEN);
 	t.post("/1/cards", {name: title, idList: process.env.HUBOT_TRELLO_LIST}, function (err, data) {
 			if (err) {
-				msg.send "ERROR"
+				msg.send("ERROR");
 			} else {
-				msg.send "「#{title}」をTrelloに保存しました。"
+				msg.send("「" + title + "」をTrelloに保存しました。");
 			}
 	});
 }
